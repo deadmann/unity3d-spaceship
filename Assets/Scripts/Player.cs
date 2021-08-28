745d13074a7a538e609ae6f50db0729028b759e7
Hassan <mimosh_pisholack@hotmail.com>
2021-08-28 10:43:47 +0430

    - Separate Shooting from Player Script. - Add sprite of fx from Warped Cave assets, which may be usable later.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,25 +6,10 @@ using Object = UnityEngine.Object;
 
 public class Player : MonoBehaviour
 {
-    private Transform _gun;
-    private float _lastShootTime;
-
     private HealthBar _healthBar;
-    
-    
-    public GameObject pfBullet;
-    public GameObject muzzlePrefab;
-    public float shootingCooldown = .25f;
-    public float bulletSpeed = 30f;
 
     public int maxHealth = 100;
     public int currentHealth;
-
-    private void Awake()
-    {
-        _gun = transform.Find("Gun");
-        _lastShootTime = 0;
-    }
 
     private void Start()
     {
@@ -32,41 +17,6 @@ public class Player : MonoBehaviour
 
         _healthBar = FindObjectOfType<HealthBar>();
         _healthBar.SetMaxHealth(maxHealth);
-    }
-
-    private void Update()
-    {
-        Shoot();
-    }
-
-    private void Shoot()
-    {
-        if (!Input.GetButton("Jump"))
-            return;
-        
-        if(_lastShootTime + shootingCooldown > Time.time)
-            return;
-        
-        _lastShootTime = Time.time;
-        ShootABullet();
-    }
-
-    private void ShootABullet()
-    {
-        var muzzleVfx = Instantiate(muzzlePrefab, _gun.transform.position, Quaternion.identity);
-        var psMuzzle = muzzleVfx.GetComponent<ParticleSystem>();
-        if (psMuzzle != null)
-        {
-            Destroy(muzzleVfx, psMuzzle.main.duration);
-        }
-        else
-        {
-            var psChild = muzzleVfx.transform.GetChild(0).GetComponent<ParticleSystem>();
-            Destroy(muzzleVfx, psChild.main.duration);
-        }
-            
-        var bullet = Instantiate(pfBullet, _gun.transform.position, Quaternion.identity);
-        bullet.GetComponent<Bullet>().Setup(Vector2.up, bulletSpeed);
     }
 
     public void TakeDamage(int damage)
---
+++ Assets/Scripts/Shooting.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class Shooting : MonoBehaviour
+    {
+        private Transform _gun;
+        private float _lastShootTime;
+        public GameObject pfBullet;
+        public GameObject muzzlePrefab;
+        public float shootingCooldown = .25f;
+        public float bulletSpeed = 30f;
+        
+        private void Awake()
+        {
+            _gun = transform.Find("Gun");
+            _lastShootTime = 0;
+        }
+        
+        private void Update()
+        {
+            Shoot();
+        }
+
+        private void Shoot()
+        {
+            if (!Input.GetButton("Jump"))
+                return;
+        
+            if(_lastShootTime + shootingCooldown > Time.time)
+                return;
+        
+            _lastShootTime = Time.time;
+            ShootABullet();
+        }
+
+        private void ShootABullet()
+        {
+            var muzzleVfx = Instantiate(muzzlePrefab, _gun.transform.position, Quaternion.identity);
+            var psMuzzle = muzzleVfx.GetComponent<ParticleSystem>();
+            if (psMuzzle != null)
+            {
+                Destroy(muzzleVfx, psMuzzle.main.duration);
+            }
+            else
+            {
+                var psChild = muzzleVfx.transform.GetChild(0).GetComponent<ParticleSystem>();
+                Destroy(muzzleVfx, psChild.main.duration);
+            }
+            
+            var bullet = Instantiate(pfBullet, _gun.transform.position, Quaternion.identity);
+            bullet.GetComponent<Bullet>().Setup(Vector2.up, bulletSpeed);
+        }
+    }
+}
